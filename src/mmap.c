6bca36207de9b95673a3ae1a7e6df7b51d87f762
Maxlufs <maxlufs@hotmail.com>
2014-02-16 18:28:52 -0800

    Com indentation @mmap.c

--- src/mmap.c
+++ src/mmap.c
@@ -23,51 +23,51 @@ int main(int argc, char *argv[]) {
 
 	int i;
 	for (i = 0; i < 1; ++i) {
-	int fd;
-	struct stat buf;
-	void* mmapp, *mmapp_bkp;
+		int fd;
+		struct stat buf;
+		void* mmapp, *mmapp_bkp;
 
-	fd = open("mmap_file", O_RDONLY);
-	if (fd == -1) {
-		perror("open");
-		exit(1);
-	}
-
-	if (fstat(fd, &buf) < 0) {
-		perror("fstat");
-		close(fd);
-		exit(1);
-	}
-
-	mmapp = mmap(0, buf.st_size, PROT_READ, MAP_SHARED, fd, 0);
-	mmapp_bkp = mmapp;
-
-	if (mmapp == MAP_FAILED) {
-		perror("mmap");
-		if (buf.st_size == 0) {
-			fprintf(stderr, "file size = 0\n");
+		fd = open("mmap_file", O_RDONLY);
+		if (fd == -1) {
+			perror("open");
+			exit(1);
 		}
+
+		if (fstat(fd, &buf) < 0) {
+			perror("fstat");
+			close(fd);
+			exit(1);
+		}
+
+		mmapp = mmap(0, buf.st_size, PROT_READ, MAP_SHARED, fd, 0);
+		mmapp_bkp = mmapp;
+
+		if (mmapp == MAP_FAILED) {
+			perror("mmap");
+			if (buf.st_size == 0) {
+				fprintf(stderr, "file size = 0\n");
+			}
+			close(fd);
+			exit(1);
+		}
+
+		int j;
+		char c;
+		ticks t1, t2;
+		for (j = 0; j < 4097; ++j) {
+			t1 = getticks();
+			c = *(char*)mmapp;
+			t2 = getticks();
+			mmapp += 1;
+			fprintf(stdout, "%llu\n", t2 - t1);
+		}
+
+		//munmap(mmapp, buf.st_size);
+		if (munmap(mmapp_bkp, buf.st_size) == -1) {
+			perror("munmap");
+		}
+		// strcpy(mmapp, "strncpy test");
 		close(fd);
-		exit(1);
-	}
-
-	int j;
-	char c;
-	ticks t1, t2;
-	for (j = 0; j < 4097; ++j) {
-		t1 = getticks();
-		c = *(char*)mmapp;
-		t2 = getticks();
-		mmapp += 1;
-		fprintf(stdout, "%llu\n", t2 - t1);
-	}
-
-	//munmap(mmapp, buf.st_size);
-	if (munmap(mmapp_bkp, buf.st_size) == -1) {
-		perror("munmap");
-	}
-	// strcpy(mmapp, "strncpy test");
-	close(fd);
 
 	}
 	return 0;

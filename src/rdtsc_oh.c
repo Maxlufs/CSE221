59e0e1f4313b2f44c1d381b037e0b03c28417954
Maxlufs <maxlufs@hotmail.com>
2014-02-17 22:57:57 -0800

    Com indentation @rdtsc_oh.c

--- src/rdtsc_oh.c
+++ src/rdtsc_oh.c
@@ -5,20 +5,20 @@
 
 int main(int argc, char *argv[])
 {
-int i;
-// float avg = 0.0;
-// int sum = 0;
+	int i;
+	// float avg = 0.0;
+	// int sum = 0;
 
-// for (i = 0; i < LOOP_COUNT; ++i)
-for (;;)
+	// for (i = 0; i < LOOP_COUNT; ++i)
+	for (;;)
 	{
 		ticks t1 = getticks();
 		ticks t2 = getticks();
 		printf("%f, %llu\n", elapsed(t2,t1), t2-t1);
-//		sum += (int)(t2-t1);
+		//		sum += (int)(t2-t1);
 	}
-// avg = sum / LOOP_COUNT;
-// printf("avg = %f\n", avg);
+	// avg = sum / LOOP_COUNT;
+	// printf("avg = %f\n", avg);
 	return 0;
 }
 
